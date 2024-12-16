62c2a331ba9380eda3c93473911a3e224e57761a
Azzeddine Remmal <31375681+azzeddine2017@users.noreply.github.com>
2024-12-16 16:43:37 +0100

    Correct path of dll file extension to bin folder

--- buildvc_x64.bat
+++ buildvc_x64.bat
@@ -2,6 +2,6 @@ cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/build/locatevc.bat x64
 cl /c /DEBUG ring_subprocess.c -I"..\..\language\include"
-link /DEBUG ring_subprocess.obj  ..\..\lib\ring.lib Shell32.lib /DLL /OUT:ring_subprocess.dll
+link /DEBUG ring_subprocess.obj  ..\..\lib\ring.lib Shell32.lib /DLL /OUT: ..\..\bin\ring_subprocess.dll
 del ring_subprocess.obj
 endlocal
